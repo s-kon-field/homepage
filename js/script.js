c817f9560396e506bbf1138ab15739f7a99157a7
s-kon-field <s-kon@lib.hokudai.ac.jp>
2024-11-08 14:17:10 +0900

    グローバルメニュー

--- js/script.js
+++ js/script.js
@@ -1,41 +1,26 @@
-
-
-//sp menu
-
-(function () {
-    document
-      .querySelector("body")
-      .insertAdjacentHTML("afterbegin", '<div class="sp-navi-box"><div class="sp-navi"></div></div>');
-    document.querySelector(".sp-navi").innerHTML = document.querySelector(
-      ".navi"
-    ).innerHTML;
-  
-    const documentElement = document.querySelector("html");
-    const contentElement = document.querySelector("#main");
-    const sidebarElement = document.querySelector(".sp-navi-box");
-    const openSidebar = function () {
-      scl_point = window.pageYOffset;
-      documentElement.classList.add("sidebar-is-open");
-      $("body").css("top", "-" + scl_point + "px");
-    };
-    const closeSidebar = function () {
-      documentElement.classList.remove("sidebar-is-open");
-      $(window).scrollTop(
-        $("body").css("top").replace(/-/g, "").replace(/px/g, "")
-      );
-      $("body").css("top", 0);
-    };
-    $(".sp-navi-toggle").click(function () {
-      //, .sp-navi-box a[href^=#]
-      if ($(".sidebar-is-open").length) {
-        closeSidebar();
+$(function(){
+    /*=================================================
+    ハンバーガーメニュー
+    解説は、「中級編：ストアサイト（インテリア）」参照
+    ===================================================*/
+    $('.hamburger').on('click', function() {
+      if ($('#header').hasClass('open')) {
+        $('#header').removeClass('open');
       } else {
-        openSidebar();
+        $('#header').addClass('open');
       }
     });
-  })();
   
+    // #maskのエリアをクリックした時にメニューを閉じる
+    $('#mask').on('click', function() {
+      $('#header').removeClass('open');
+    });
   
+    // リンクをクリックした時にメニューを閉じる
+    $('#navi a').on('click', function() {
+      $('#header').removeClass('open');
+    });
+
 
 $(".slide-items").slick({
     autoplay: true, // 自動再生
@@ -56,4 +41,4 @@ $(".slide-items").slick({
           }
         },
       ]
-    });  
+    });  })
