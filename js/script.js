e1431c401c212143a4c6446b866e1b2cf95edb62
s-kon-field <s-kon@lib.hokudai.ac.jp>
2024-11-09 00:17:31 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -22,25 +22,40 @@ $(function(){
     $('#header').removeClass('open');
   });
 
-$(".slide-items").slick({
+  $('.slide-items').slick({
     autoplay: true, // 自動再生
-    centerMode: true,
-  variableWidth: true,
     arrows: true, // 矢印
-    dots: true, // インジケーター
+    dots: true,
+    infinite: false,
+    speed: 600,
+    slidesToShow: 4,
+    slidesToScroll: 4,
     responsive: [
-        {
-          breakpoint: 768,
-          settings: {
-            arrows: false,
-          }
-        },
-        {
-          breakpoint: 640,
-          settings: {
-            arrows: false,
-            dots: false,
-          }
-        },
-      ]
-    });  })
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  })})
