47e5ee6735af778e6399ac189f9c946817294942
s-kon-field <s-kon@lib.hokudai.ac.jp>
2024-11-08 12:05:33 +0900

    へっだー

--- js/script.js
+++ js/script.js
@@ -1,3 +1,42 @@
+
+
+//sp menu
+
+(function () {
+    document
+      .querySelector("body")
+      .insertAdjacentHTML("afterbegin", '<div class="sp-navi-box"><div class="sp-navi"></div></div>');
+    document.querySelector(".sp-navi").innerHTML = document.querySelector(
+      ".navi"
+    ).innerHTML;
+  
+    const documentElement = document.querySelector("html");
+    const contentElement = document.querySelector("#main");
+    const sidebarElement = document.querySelector(".sp-navi-box");
+    const openSidebar = function () {
+      scl_point = window.pageYOffset;
+      documentElement.classList.add("sidebar-is-open");
+      $("body").css("top", "-" + scl_point + "px");
+    };
+    const closeSidebar = function () {
+      documentElement.classList.remove("sidebar-is-open");
+      $(window).scrollTop(
+        $("body").css("top").replace(/-/g, "").replace(/px/g, "")
+      );
+      $("body").css("top", 0);
+    };
+    $(".sp-navi-toggle").click(function () {
+      //, .sp-navi-box a[href^=#]
+      if ($(".sidebar-is-open").length) {
+        closeSidebar();
+      } else {
+        openSidebar();
+      }
+    });
+  })();
+  
+  
+
 $(".slide-items").slick({
     autoplay: true, // 自動再生
     arrows: true, // 矢印
@@ -18,4 +57,3 @@ $(".slide-items").slick({
         },
       ]
     });  
-
